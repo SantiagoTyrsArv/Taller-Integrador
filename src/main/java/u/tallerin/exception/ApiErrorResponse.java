65bc850c254fec58876896b153287329e2f36cfc
Jose B <joseluisburbano19105@gmail.com>
2026-04-23 18:25:13 -0500

    exceptiongs

---
+++ src/main/java/u/tallerin/exception/ApiErrorResponse.java
@@ -0,0 +1,18 @@
+package u.tallerin.exception;
+
+import java.time.OffsetDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiErrorResponse {
+    private OffsetDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+}
---
+++ src/main/java/u/tallerin/exception/BusinessException.java
@@ -0,0 +1,8 @@
+package u.tallerin.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
