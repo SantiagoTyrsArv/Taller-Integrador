bae61c1945dfb3b95c78e52f2db7322498db31ac
Santiago Arevalo <santif.arevalo@gmail.com>
2026-04-23 17:27:07 -0500

    Initial commit

---
+++ src/main/java/u/tallerin/TallerInApplication.java
@@ -0,0 +1,13 @@
+package u.tallerin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/u/tallerin/TallerInApplicationTests.java
@@ -0,0 +1,13 @@
+package u.tallerin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
