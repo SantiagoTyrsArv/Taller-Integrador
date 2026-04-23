f5e0a7b5df34a8b593e3ddf387c8759a087fce88
Santiago Arevalo <santif.arevalo@gmail.com>
2026-04-23 17:37:29 -0500

    Add UML

--- src/main/java/u/tallerin/TallerInApplication.java
+++ src/main/java/u/tallerin/TallerInApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TallerInApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TallerInApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TallerInApplication.class, args);
+    }
 
 }
--- src/test/java/u/tallerin/TallerInApplicationTests.java
+++ src/test/java/u/tallerin/TallerInApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TallerInApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
